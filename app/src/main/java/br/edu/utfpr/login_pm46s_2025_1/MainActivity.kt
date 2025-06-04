b4385b17e7d696babb4f7e47520ddaf1926a0f03
robisoncrisbrito <robison.brito@gmail.com>
2025-06-04 19:17:38 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/utfpr/login_pm46s_2025_1/MainActivity.kt
@@ -0,0 +1,16 @@
+package br.edu.utfpr.login_pm46s_2025_1
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun btLoginOnClick(view: View) {
+
+    }
+}
