703c7b09a2c96350258ea203d443fd6f960fc973
robisoncrisbrito <robison.brito@gmail.com>
2025-06-26 13:04:24 -0400

    Com login

--- app/src/main/java/br/edu/utfpr/login_pm46s_2025_1/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/login_pm46s_2025_1/MainActivity.kt
@@ -1,16 +1,103 @@
 package br.edu.utfpr.login_pm46s_2025_1
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.credentials.Credential
+import androidx.credentials.CredentialManager
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import androidx.lifecycle.lifecycleScope
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential.Companion.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.auth
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var credentialManager: CredentialManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        auth = Firebase.auth
+
+        credentialManager = CredentialManager.create(this)
+
         setContentView(R.layout.activity_main)
+
+    }
+
+    private  fun launchCredentialManager() {
+        val googleIdOption = GetGoogleIdOption.Builder()
+            // Your server's client ID, not your Android client ID.
+            .setServerClientId(getString(R.string.default_web_client_id))
+            // Only show accounts previously used to sign in.
+            .setFilterByAuthorizedAccounts(false)
+            .build()
+
+        // Create the Credential Manager request
+        val request = GetCredentialRequest.Builder()
+            .addCredentialOption(googleIdOption)
+            .build()
+        // [END create_credential_manager_request]
+
+        lifecycleScope.launch {
+            try {
+                // Launch Credential Manager UI
+                val result = credentialManager.getCredential(
+                    context = this@MainActivity,
+                    request = request
+                )
+
+                // Extract credential from the result returned by Credential Manager
+                handleSignIn(result.credential)
+
+            } catch (e: Exception) {
+                Log.e("Erro", "Couldn't retrieve user's credentials: ${e.localizedMessage}")
+            }
+        }
+    }
+
+    private fun handleSignIn(credential: Credential) {
+        // Check if credential is of type Google ID
+        if (credential is CustomCredential && credential.type == TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+            // Create Google ID Token
+            val googleIdTokenCredential = GoogleIdTokenCredential.createFrom(credential.data)
+
+            // Sign in to Firebase with using the token
+            firebaseAuthWithGoogle(googleIdTokenCredential.idToken)
+        } else {
+            Log.w("Erro", "Credential is not of type Google ID!")
+        }
+    }
+
+    // [START auth_with_google]
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("Erro", "signInWithCredential:success")
+                    val email = auth.currentUser?.email
+                    val user = auth.currentUser
+                    Toast.makeText(this, "Usuário: $email - $user", Toast.LENGTH_SHORT).show()
+                } else {
+                    // If sign in fails, display a message to the user
+                    Log.w( "Erro", "signInWithCredential:failure", task.exception)
+                }
+            }
     }
 
     fun btLoginOnClick(view: View) {
-
+        launchCredentialManager()
     }
 }
